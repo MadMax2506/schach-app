b64357b3b23231953c00c09c948d6353b6b21d43
Chr1zM <ch.me010103@gmail.com>
2023-05-02 23:25:47 +0200

    Grundlegenden Aufbau implementieren #3 Zwischenstand * Layout Anpassungen

--- app/src/main/java/janorschke/meyer/game/GameActivity.kt
+++ app/src/main/java/janorschke/meyer/game/GameActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import janorschke.meyer.R
 import janorschke.meyer.databinding.ActivityGameBinding
 import janorschke.meyer.global.TransferKeys
 import janorschke.meyer.home.MainActivity
@@ -13,6 +14,7 @@ const val LOG_TAG = "GameActivity"
 
 class GameActivity : AppCompatActivity() {
     private lateinit var binding: ActivityGameBinding
+    private lateinit var gameFieldAdapter: GameFieldAdapter
     private lateinit var boardViewModel: BoardViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,9 @@ class GameActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         boardViewModel = ViewModelProvider(this)[BoardViewModel::class.java]
-        val gameFieldAdapter = GameFieldAdapter(boardViewModel)
+        gameFieldAdapter = GameFieldAdapter(boardViewModel)
         binding.board?.adapter = gameFieldAdapter
+        binding.namePlayer1?.text = getString(R.string.placeholder, "Player 1")
 
         val gameModeStr = intent.extras?.getString(TransferKeys.GAME_MODE.value)
         if (gameModeStr == null) {
@@ -31,6 +34,7 @@ class GameActivity : AppCompatActivity() {
         } else {
             val gameMode = GameMode.valueOf(gameModeStr)
             // TODO get game mode specific things, aiLevel ....
+            binding.namePlayer2?.text = getString(R.string.placeholder, "Player 2")
         }
     }
 }
