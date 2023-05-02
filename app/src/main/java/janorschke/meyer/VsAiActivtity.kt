72d00fa5d9a272d913a7395cfeffff5f58c50d13
Chr1zM <ch.me010103@gmail.com>
2023-05-02 16:50:21 +0200

    Grundlegenden Aufbau implementieren #3 * Erste Umsetzung * Startseite im Land und Portraitmodus umgesetzt * KI Seite im Land und Portraitmodus umgesetzt

--- app/src/main/java/janorschke/meyer/VsAiActivtity.kt
+++ app/src/main/java/janorschke/meyer/VsAiActivtity.kt
@@ -2,11 +2,14 @@ package janorschke.meyer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import janorschke.meyer.databinding.ActivityVsaiBinding
 
 class VsAiActivtity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityVsaiBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityVsaiBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
--- app/src/main/java/janorschke/meyer/VsOnlinePlayer.kt
+++ app/src/main/java/janorschke/meyer/VsOnlinePlayer.kt
@@ -2,10 +2,14 @@ package janorschke.meyer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import janorschke.meyer.databinding.ActivityVsonlineplayerBinding
 
 class VsOnlinePlayer : AppCompatActivity() {
+    private lateinit var binding: ActivityVsonlineplayerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityVsonlineplayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
