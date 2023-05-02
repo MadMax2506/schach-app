29bfb606068ecf3fc2017d52b106315b6a610bec
Chr1zM <ch.me010103@gmail.com>
2023-05-02 23:47:27 +0200

    Grundlegenden Aufbau implementieren #3 Zwischenstand * getView und PieceType Umbau

--- app/src/main/java/janorschke/meyer/game/GameFieldAdapter.kt
+++ app/src/main/java/janorschke/meyer/game/GameFieldAdapter.kt
@@ -29,10 +29,27 @@ class GameFieldAdapter(private val viewModel: BoardViewModel) : BaseAdapter() {
         } else {
             convertView.findViewById(R.id.btn)
         }
+
+        val content = viewModel.getFieldContent(position)
+        when(content)  {
+            PieceType.PAWN_WHITE -> button.setBackgroundResource(R.drawable.chess_plt45)
+            PieceType.PAWN_BLACK -> button.setBackgroundResource(R.drawable.chess_pdt45)
+            PieceType.KNIGHT_WHITE -> button.setBackgroundResource(R.drawable.chess_nlt45)
+            PieceType.KNIGHT_BLACK -> button.setBackgroundResource(R.drawable.chess_ndt45)
+            PieceType.BISHOP_WHITE -> button.setBackgroundResource(R.drawable.chess_blt45)
+            PieceType.BISHOP_BLACK -> button.setBackgroundResource(R.drawable.chess_bdt45)
+            PieceType.ROOK_WHITE -> button.setBackgroundResource(R.drawable.chess_rlt45)
+            PieceType.ROOK_BLACK -> button.setBackgroundResource(R.drawable.chess_rdt45)
+            PieceType.QUEEN_WHITE -> button.setBackgroundResource(R.drawable.chess_qlt45)
+            PieceType.QUEEN_BLACK -> button.setBackgroundResource(R.drawable.chess_qdt45)
+            PieceType.KING_WHITE -> button.setBackgroundResource(R.drawable.chess_klt45)
+            PieceType.KING_BLACK -> button.setBackgroundResource(R.drawable.chess_kdt45)
+            else -> {button.setBackgroundResource(R.color.purple_200)} // TODO
+        }
+
         button.setOnClickListener {
             viewModel.onFieldClicked(position)
         }
-        button.text = viewModel.getFieldContent(position).value
         return button
     }
 }
--- app/src/main/java/janorschke/meyer/game/PieceType.kt
+++ app/src/main/java/janorschke/meyer/game/PieceType.kt
@@ -2,11 +2,18 @@ package janorschke.meyer.game
 
 enum class PieceType(val value: String) {
     NONE("NONE"),
-    PAWN("PAWN"),
-    BISHOP("BISHOP"),
-    KNIGHT("KNIGHT"),
-    ROOK("ROOK"),
-    QUEEN("QUEEN"),
-    KING("KING")
-    
+
+    PAWN_BLACK("PAWN_BLACK"),
+    BISHOP_BLACK("BISHOP_BLACK"),
+    KNIGHT_BLACK("KNIGHT_BLACK"),
+    ROOK_BLACK("ROOK_BLACK"),
+    QUEEN_BLACK("QUEEN_BLACK"),
+    KING_BLACK("KING_BLACK"),
+
+    PAWN_WHITE("PAWN_WHITE"),
+    BISHOP_WHITE("BISHOP_WHITE"),
+    KNIGHT_WHITE("KNIGHT_WHITE"),
+    ROOK_WHITE("ROOK_WHITE"),
+    QUEEN_WHITE("QUEEN_WHITE"),
+    KING_WHITE("KING_WHITE")
 }
