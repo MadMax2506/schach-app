51ac369a9bad5798ce28aa9d13389618b05c7fc5
Chr1zM <ch.me010103@gmail.com>
2023-05-04 22:25:49 +0200

    Mögliche Züge eines Springers berechnen #15 * Piece erweitert um Validierungsmethode * possibleMoves implementiert

--- app/src/main/java/janorschke/meyer/game/piece/Knight.kt
+++ app/src/main/java/janorschke/meyer/game/piece/Knight.kt
@@ -2,6 +2,7 @@ package janorschke.meyer.game.piece
 
 import janorschke.meyer.R
 import janorschke.meyer.game.BoardViewModel
+import kotlin.math.abs
 
 class Knight(boardViewModel: BoardViewModel, color: PieceColor) : Piece(boardViewModel, color) {
     override fun getImageId(): Int {
@@ -9,6 +10,19 @@ class Knight(boardViewModel: BoardViewModel, color: PieceColor) : Piece(boardVie
     }
 
     override fun possibleMoves(position: PiecePosition): MutableCollection<PiecePosition> {
-        TODO("Not yet implemented")
+        // TODO steht eigener König im Schach vor dem bewegen?
+
+        val possibleMoves = mutableListOf<PiecePosition>()
+        for (i in -2..2) {
+            for (j in -2..2) {
+                // proves that the position can be reached by the knight
+                if (abs(i) + abs(j) != 3) continue
+                val currentPosition = PiecePosition(position.row + i, position.col + j)
+                if (!isFieldAvailable(currentPosition)) continue
+                possibleMoves.add(currentPosition)
+            }
+        }
+        // TODO steht eigener König im Schach nach dem bewegen?
+        return possibleMoves
     }
 }
--- app/src/main/java/janorschke/meyer/game/piece/Piece.kt
+++ app/src/main/java/janorschke/meyer/game/piece/Piece.kt
@@ -4,6 +4,20 @@ import janorschke.meyer.game.BoardViewModel
 
 abstract class Piece(val boardViewModel: BoardViewModel, val color: PieceColor) {
     /**
+     * @param position being checked
+     * @return true, if a piece can move to the position
+     */
+    protected fun isFieldAvailable(position: PiecePosition): Boolean {
+        val board = boardViewModel.getBoard()
+
+        val indices = board.indices
+        if (position.row !in indices && position.col !in indices) return false
+
+        val piece = board[position.row][position.col]
+        return if (piece == null) true else piece.color != this.color
+    }
+
+    /**
      * @return the image id for the piece
      */
     abstract fun getImageId(): Int
@@ -13,4 +27,5 @@ abstract class Piece(val boardViewModel: BoardViewModel, val color: PieceColor) 
      * @return possible moves
      */
     abstract fun possibleMoves(position: PiecePosition): MutableCollection<PiecePosition>
+
 }
