4f5381abdf8f1063fd3378b07bd8854dd7c74319
Max Janorschke <programming@max-janorschke.de>
2023-05-04 21:52:14 +0200

    Implement rest of game layout #14
    * add default player name as string
    * remove fragment
    * create own player info layout
    * set player names
    * define new dim
    * extract board view to an own layout
    * implement board and player info for the game layout

--- app/src/main/java/janorschke/meyer/game/GameActivity.kt
+++ app/src/main/java/janorschke/meyer/game/GameActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import janorschke.meyer.R
 import janorschke.meyer.ai.AiLevel
 import janorschke.meyer.databinding.ActivityGameBinding
 import janorschke.meyer.global.TransferKeys
@@ -24,12 +25,9 @@ class GameActivity : AppCompatActivity() {
         val boardViewModel = ViewModelProvider(this)[BoardViewModel::class.java]
         val gameFieldAdapter = GameFieldAdapter(applicationContext, boardViewModel)
 
-        binding.board?.adapter = gameFieldAdapter
+        binding.boardWrapper?.board?.adapter = gameFieldAdapter
 
         // player handling
-        // TODO set names
-        // TODO set adapters for moves and geschlagene figuren
-
         val aiLevelString = intent.extras?.getString(TransferKeys.AI_LEVEL.value)
         if (aiLevelString == null) {
             startActivity(Intent(this, MainActivity::class.java))
@@ -38,9 +36,8 @@ class GameActivity : AppCompatActivity() {
             val aiLevel = enumValueOf<AiLevel>(aiLevelString)
             Log.d(LOG_TAG, "Start game with ai-level=${aiLevel}")
 
-            binding.namePlayer1?.text = "TODO"
-            binding.namePlayer2?.text = resources.getString(aiLevel.resourceId)
-            // TODO get game mode specific things, aiLevel, player name ....
+            binding.playerOne?.name?.text = resources.getString(aiLevel.resourceId)
+            binding.playerTwo?.name?.text = resources.getString(R.string.default_player_name)
         }
     }
 }
