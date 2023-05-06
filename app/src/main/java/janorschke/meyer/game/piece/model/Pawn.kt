57c29482d6757923de5c161bc8b398eb943877b0
Chr1zM <ch.me010103@gmail.com>
2023-05-06 18:15:39 +0200

    Anklicken eines Feldes behandeln #43 * Bauer Fix

--- app/src/main/java/janorschke/meyer/game/piece/model/Pawn.kt
+++ app/src/main/java/janorschke/meyer/game/piece/model/Pawn.kt
@@ -8,22 +8,23 @@ import janorschke.meyer.game.piece.PiecePosition
 class Pawn(board: Board, color: PieceColor) : Piece(board, color, PieceInfo.PAWN) {
     override fun possibleMoves(position: PiecePosition): MutableList<PiecePosition> {
         val possibleMoves = mutableListOf<PiecePosition>()
+        val moveDirection = if (color == PieceColor.WHITE) -1 else 1
 
         // TODO Schach?
 
         // normal move
-        PiecePosition(position.row + 1, position.col).apply {
+        PiecePosition(position.row + moveDirection, position.col).apply {
             if (fieldValidation.isEmpty(this)) possibleMoves.add(this)
         }
 
         // move from base line
-        PiecePosition(position.row + 2, position.col).apply {
+        PiecePosition(position.row + 2 * moveDirection, position.col).apply {
             if (!moved && fieldValidation.isEmpty(this)) possibleMoves.add(this)
         }
 
         // beat
         for (i in arrayOf(-1, 1)) {
-            PiecePosition(position.row + 1, position.col + i).apply {
+            PiecePosition(position.row + moveDirection, position.col + i).apply {
                 if (!isFieldUnavailable(this) && fieldValidation.isOpponent(this)) possibleMoves.add(this)
             }
         }
