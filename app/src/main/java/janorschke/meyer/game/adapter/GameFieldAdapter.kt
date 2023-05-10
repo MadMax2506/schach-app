eb59dd8c1d6f40a2c441b855056121d4b5ee0a7f
Max Janorschke <max.janorschke@veda.net>
2023-05-10 14:49:38 +0200

    PossibleMove auf Board anzeigen beim Schlagen einer gegnerischen Figur #51
    * show beat indicator in the background

--- app/src/main/java/janorschke/meyer/game/adapter/GameFieldAdapter.kt
+++ app/src/main/java/janorschke/meyer/game/adapter/GameFieldAdapter.kt
@@ -3,6 +3,8 @@ package janorschke.meyer.game.adapter
 import android.content.Context
 import android.graphics.Color
 import android.graphics.ColorMatrixColorFilter
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.LayerDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,6 +21,7 @@ import janorschke.meyer.game.piece.model.Piece
 
 class GameFieldAdapter(private val context: Context, private val gameViewModel: GameViewModel) : BaseAdapter() {
     private class ViewHolder(val binding: GameFieldBinding, val view: View)
+
     private var possibleMoves: List<PiecePosition> = emptyList()
 
     override fun getCount(): Int {
@@ -49,28 +52,19 @@ class GameFieldAdapter(private val context: Context, private val gameViewModel: 
         }
 
         val position = PiecePosition(index)
+        val piece = gameViewModel.getField(position)
+
+        // field background
         holder.view.setBackgroundResource(getViewBackgroundColor(position))
-        if(possibleMoves.contains(position)) {
+
+        if (possibleMoves.contains(position)) {
             holder.binding.btn.background = ContextCompat.getDrawable(context, R.drawable.chess_possiblemove)!!.mutate()
         } else {
             holder.binding.btn.setBackgroundColor(Color.TRANSPARENT)
         }
 
-        val piece = gameViewModel.getField(position)
-        if (piece != null) {
-            ContextCompat.getDrawable(context, piece.pieceInfo.imageId)!!.mutate().apply {
-                if (piece.color == PieceColor.BLACK) {
-                    val NEGATIVE = floatArrayOf(
-                            -1.0f, 0f, 0f, 0f, 255f,  // red
-                            0f, -1.0f, 0f, 0f, 255f,  // green
-                            0f, 0f, -1.0f, 0f, 255f,  // blue
-                            0f, 0f, 0f, 1.0f, 0f // alpha
-                    )
-                    this.colorFilter = ColorMatrixColorFilter(NEGATIVE)
-                }
-                holder.binding.btn.background = this
-            }
-        }
+
+        holder.binding.btn.background = getPieceBackground(piece, position)
         holder.binding.btn.setOnClickListener { gameViewModel.onFieldClicked(position) }
 
         return holder.view
@@ -78,5 +72,44 @@ class GameFieldAdapter(private val context: Context, private val gameViewModel: 
 
     private fun getViewBackgroundColor(position: PiecePosition): Int {
         return if (position.row % 2 != position.col % 2) R.color.brown else R.color.beige
+    }
+
+    private fun getPieceBackground(piece: Piece?, position: PiecePosition): Drawable? {
+        if (piece == null) {
+            return if (possibleMoves.contains(position)) {
+                ContextCompat.getDrawable(context, R.drawable.chess_possiblemove)!!.mutate()
+            } else null
+        }
+
+        val pieceImage = ContextCompat.getDrawable(context, piece.pieceInfo.imageId)!!.mutate().also { drawable ->
+            if (piece.color == PieceColor.BLACK) {
+                floatArrayOf(
+                        -1.0f, 0f, 0f, 0f, 255f,  // red
+                        0f, -1.0f, 0f, 0f, 255f,  // green
+                        0f, 0f, -1.0f, 0f, 255f,  // blue
+                        0f, 0f, 0f, 1.0f, 0f // alpha
+                ).apply { drawable.colorFilter = ColorMatrixColorFilter(this) }
+            }
+        }
+
+        val layers = mutableListOf<Drawable>()
+        // Add beat indicator at the bottom
+        if (possibleMoves.contains(position)) {
+            ContextCompat.getDrawable(context, R.drawable.chess_possiblemove)!!
+                    .mutate()
+                    .also { drawable ->
+                        floatArrayOf(
+                                1f, 0.16f, 0.14f, 1f, 1f,  // red
+                                0f, 0f, 0f, 0f, 0f,  // green
+                                0f, 0f, 0f, 0f, 0f,  // blue
+                                1f, 1f, 1f, 1f, 1f // alpha
+                        ).apply { drawable.colorFilter = ColorMatrixColorFilter(this) }
+                    }.apply { layers.add(this) }
+        }
+
+        // Add figure at the top
+        layers.add(pieceImage)
+
+        return LayerDrawable(layers.toTypedArray())
     }
 }
