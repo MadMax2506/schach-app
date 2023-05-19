0e52f5afd57db44e3bfcb69100a115b5379500a4
Max Janorschke <programming@max-janorschke.de>
2023-05-19 19:34:43 +0200

    WIP Create prototype of evaluation method #93
    * create base structure

--- app/src/main/java/janorschke/meyer/service/repository/ai/AiRepository.kt
+++ app/src/main/java/janorschke/meyer/service/repository/ai/AiRepository.kt
@@ -6,6 +6,7 @@ import janorschke.meyer.enums.PieceColor
 import janorschke.meyer.service.model.game.ai.AiBoardEvaluation
 import janorschke.meyer.service.model.game.board.Board
 import janorschke.meyer.service.model.game.board.Move
+import janorschke.meyer.service.utils.piece.PieceSequence
 
 private const val LOG_TAG = "AiRepository"
 
@@ -25,5 +26,29 @@ abstract class AiRepository(val color: PieceColor, protected val board: Board, v
         Log.d(LOG_TAG, "Evaluate the board")
 
         TODO("evaluateBoard is not implemented")
+        generateMoves().map { move -> moveEvaluation(move) }
+    }
+
+    private fun generateMoves(): MutableList<Move> {
+        return PieceSequence.allPiecesByColor(board, color)
+                .map { indexedPiece ->
+                    indexedPiece.piece
+                            .possibleMoves(board, indexedPiece.position)
+                            .map { possibleMove -> Board(board).createBoardMove(indexedPiece.position, possibleMove) }
+                }
+                .flatten()
+                .toMutableList()
+    }
+
+    private fun moveEvaluation(move: Move) {
+        TODO("moveEvaluation https://www.freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/")
+    }
+
+    private fun searchTreeMinMax() {
+        TODO("searchTreeMinMax https://www.freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/")
+    }
+
+    private fun alphaBetaTrimming() {
+        TODO("alphaBetaTrimming https://www.freecodecamp.org/news/simple-chess-ai-step-by-step-1d55a9266977/")
     }
 }
