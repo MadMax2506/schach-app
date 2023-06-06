e31d8be11b4899b7e4a369ae098e73ffe55e332d
Max Janorschke <programming@max-janorschke.de>
2023-06-06 18:51:01 +0200

    Optimize tree genration with minimax #111
    * add todos

--- app/src/main/java/janorschke/meyer/ChessApplication.kt
+++ app/src/main/java/janorschke/meyer/ChessApplication.kt
@@ -4,4 +4,6 @@ import android.app.Application
 
 private const val LOG_TAG = "ChessApplication"
 
+// TODO https://github.com/MadMax2506/android-wahlmodul-project/issues/125
+
 class ChessApplication : Application()
